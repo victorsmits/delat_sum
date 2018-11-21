8ab33e8a01c4699ac06bd791aec88fa55abe6522
victorsmits <victor.smits@student.vinci.be>
2018-11-21 11:59:05 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/delat_sum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/delat_sum.dir/main.c.o"
+  "delat_sum.pdb"
+  "delat_sum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/delat_sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
